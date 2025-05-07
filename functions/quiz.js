1526e546794835cc43ce975c08c09a793071c109
MdUmar <risingsheikh@gmail.com>
2025-05-08 01:08:35 +0530

    Minor change 2.0

--- functions/quiz.js
+++ functions/quiz.js
@@ -12,7 +12,8 @@ var score = 0;
 let data = {};
 
 // app.use(express.static("public"));
-app.set("view engine", "ejs");
+// app.set("view engine", "ejs");
+app.set("views", path.join(process.cwd(), "views"));
 // app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
